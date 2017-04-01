d79cc758323bcfbb118c34051ae56d6c2d0b1a02
Arty Buldauskas <abuldauskas@wayfair.com>
2017-03-31 22:33:00 -0400

    pixi tutorial

--- src/main.js
+++ src/main.js
@@ -2,11 +2,25 @@ import pixi from './renderer';
 const { autoDetectRenderer, Container } = pixi;
 
 const appRoot = document.getElementById('app');
-const renderer = autoDetectRenderer(255, 255);
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 600;
+const CANVAS_OPTIONS = {
+  antialias: false,
+  transparent: true,
+  resolution: 1
+};
+
+const renderer = autoDetectRenderer(
+  CANVAS_WIDTH,
+  CANVAS_HEIGHT,
+  CANVAS_OPTIONS
+);
+renderer.view.style.border = '1px dashed black';
 
 appRoot.appendChild(renderer.view);
 
 const stage = new Container();
+
 renderer.render(stage);
 
 
